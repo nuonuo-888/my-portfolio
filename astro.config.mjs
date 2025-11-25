452909a292414c6045a94413a26e8787f11bb6a3
nuonuo-888 <2720161028@qq.com>
2025-11-25 17:47:25 +0800

    chore: add edgeone adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
+import edgeoneAdapter from "@edgeone/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), edgeoneAdapter()],
   },
-
+  adapter: edgeoneAdapter(),
   integrations: [react()]
 });
